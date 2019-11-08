e560c161c451dbf7a644d3be0fe7405a3ddf5ef6
Bharat Agarwal <agarwalbharat68@gmail.com>
2019-11-09 03:48:24 +0530

    menu chips added

--- src/components/common/menu.vue
+++ src/components/common/menu.vue
@@ -1,27 +1,38 @@
 <template>
-    <v-container fluid class="text-center">
+    <div class="Menu">
+        <v-container fluid>
         <div class="pt-12" v-if="Object.keys(menu).length==0">
             <v-progress-circular
-        class=""
-      :size="50"
-      :width="5"
-      color="purple"
-      indeterminate
-    ></v-progress-circular>
+            class=""
+            :size="50"
+            :width="5"
+            color="purple"
+            indeterminate
+            ></v-progress-circular>
         </div>
-        <h1 v-else>
+        <div v-else>
             <p class="text-center display-1 pt-5 font-weight-medium">{{ menu.day }}</p>
-            {{ menu }}</h1>
+            <MenuChips :menu="menu.breakfast.sort((a, b) => a.length - b.length)" name="Breakfast" :timing="menu.timings.breakfast"/>
+            <MenuChips :menu="menu.lunch.sort((a, b) => a.length - b.length)"  name="Lunch" :timing="menu.timings.lunch"/>
+            <MenuChips :menu="menu.snacks.sort((a, b) => a.length - b.length)" name="Snacks" :timing="menu.timings.snacks"/>
+            <MenuChips :menu="menu.dinner.sort((a, b) => a.length - b.length)" name="Dinner" :timing="menu.timings.dinner"/>
+        </div>
     </v-container>
+    </div>
 </template>
 
 <script>
+import MenuChips from "@/components/menu/menuchips";
+
 export default {
     name:"Menu",
     props: {
         menu:Object,
         isLoading:Boolean
     },
+    components:{
+        MenuChips
+    }
 
 }
 </script>
---
+++ src/components/menu/menuchips.vue
@@ -0,0 +1,33 @@
+<template>
+    <div :class="name">
+        <p class="text-left font-weight-medium">{{ name }}</p>
+       <v-chip-group
+        multiple
+        column
+        >
+        <v-chip v-for="tag in menu" :key="tag" pill>
+            <v-avatar left>
+              <img src="https://randomuser.me/api/portraits/men/35.jpg" alt="trevor">
+            </v-avatar>
+          {{ tag }}
+        </v-chip>
+      </v-chip-group>
+      
+    </div>
+</template>
+
+<script>
+export default {
+    name:"MenuChips",
+    props:{
+        menu:Array,
+        name:String,
+        timing:String
+    }
+
+}
+</script>
+
+<style>
+
+</style>
